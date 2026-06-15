64f6046672666d6004bcf0f94dc73ae5915d63bb
abdulloh1995yoq-vvv <abdulloh1995yoq@gmail.com>
2026-06-15 10:21:14 +0500

    Add AI task: Bank hisob raqamini tekshiruvchi dastur

---
+++ ai_topshiriqlar/Bank_hisob_raqamini_tekshiruvchi_dastur.py
@@ -0,0 +1,9 @@
+# Bank hisob raqamini tekshiruvchi dastur
+# Kurs: Dasturlash / IT
+# Mavzu: 1-mavzu: Python va dasturlashga kirish
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+hisob_raqami = input().split()
+hisob_raqami = list(map(int,  input().split()))
