a7b4bcc4222a5ba04232edd2a9691617a1485525
abdulloh1995yoq-vvv <abdulloh1995yoq@gmail.com>
2026-06-29 10:33:27 +0500

    Add AI task: Valyuta almashish dasturi

---
+++ ai_topshiriqlar/Valyuta_almashish_dasturi.py
@@ -0,0 +1,20 @@
+# Valyuta almashish dasturi
+# Kurs: Dasturlash / IT
+# Mavzu: Sonlar: int va float — butun va kasr sonlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+miqdor = float(input())
+qolgan = input().strip().lower()
+
+kurs = 0.8872
+
+if qolgan == "dollar":
+    t = input().strip().lower()
+    if t == "eyvro":
+        natija = miqdor * kurs
+        print("%.2f" % natija)
+        
+elif qolgan == "eyvrodollar":
+    natija = miqdor / kurs 
+    print("%.2f" % natija)
