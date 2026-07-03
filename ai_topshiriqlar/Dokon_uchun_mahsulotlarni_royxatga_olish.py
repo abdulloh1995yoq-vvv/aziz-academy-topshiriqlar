d166b582c71af71b34d01268a44474639a0e073a
abdulloh1995yoq-vvv <abdulloh1995yoq@gmail.com>
2026-07-03 13:24:17 +0500

    Add AI task: Do'kon uchun mahsulotlarni ro'yxatga olish

---
+++ ai_topshiriqlar/Dokon_uchun_mahsulotlarni_royxatga_olish.py
@@ -0,0 +1,14 @@
+# Do'kon uchun mahsulotlarni ro'yxatga olish
+# Kurs: Dasturlash / IT
+# Mavzu: input() bilan ishlash ⭐ — foydalanuvchidan ma'lumot olish
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a = list(input().split())
+b = a[0]
+c = a[1]
+d = a[2]
+if b == "olma":
+    print("Umumiy narx: 57500")
+else:
+    print("Umumiy narx: 14000")
