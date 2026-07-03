3d6b697c9f38a0918ad9e80926c53c75406211c8
abdulloh1995yoq-vvv <abdulloh1995yoq@gmail.com>
2026-07-03 10:46:03 +0500

    Add: Profil kartasi [abdulloh1995yoq-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
@@ -0,0 +1,11 @@
+ism = "Aziz"
+familya = "Karimov"
+kurs = "Python"
+daraja = "Boshlang'ich"
+shahar = "Toshkent"
+print("---PROFIL---")
+print("Ism:", ism)
+print("Familya:", familya)
+print("Kurs:", kurs)
+print("Daraja:", daraja)
+print("Shahar:", shahar)
