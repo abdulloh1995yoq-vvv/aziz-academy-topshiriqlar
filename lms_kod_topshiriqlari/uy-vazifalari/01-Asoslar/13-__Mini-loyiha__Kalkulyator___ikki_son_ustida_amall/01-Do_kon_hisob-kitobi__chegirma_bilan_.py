e9363ed22f935a15bd2b9f149384efac9f30686e
abdulloh1995yoq-vvv <abdulloh1995yoq@gmail.com>
2026-07-03 12:00:40 +0500

    Add: Do'kon hisob-kitobi (chegirma bilan) [abdulloh1995yoq-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Do_kon_hisob-kitobi__chegirma_bilan_.py
@@ -0,0 +1,11 @@
+son = int(input())
+narxi = int(input())
+foiz = int(input())
+
+umumiy = son * narxi
+chegirma = (umumiy * foiz) // 100
+yakuniy = umumiy - chegirma
+
+print(umumiy)
+print(chegirma)
+print(yakuniy)
