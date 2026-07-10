eba10222adf61b8870b5777776e51f71c24ee193
abdulloh1995yoq-vvv <abdulloh1995yoq@gmail.com>
2026-07-10 13:37:14 +0500

    Add: Unli harflar soni [abdulloh1995yoq-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/24-__Amaliy__Matn_tahlili___so_z_sanash__harf_statist/01-Unli_harflar_soni.py
@@ -0,0 +1,11 @@
+matn = input().lower()
+
+unlilar_soni = (
+    matn.count('a') +
+    matn.count('e') +
+    matn.count('i') +
+    matn.count('o') +
+    matn.count('u')
+)
+
+print(unlilar_soni)
