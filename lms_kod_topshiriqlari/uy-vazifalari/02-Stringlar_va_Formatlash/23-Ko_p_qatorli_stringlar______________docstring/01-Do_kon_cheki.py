ce31fc2d8d699ca7564e5bc5f76e7371a45b54db
abdulloh1995yoq-vvv <abdulloh1995yoq@gmail.com>
2026-07-10 13:29:19 +0500

    Add: Do'kon cheki [abdulloh1995yoq-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/23-Ko_p_qatorli_stringlar______________docstring/01-Do_kon_cheki.py
@@ -0,0 +1,12 @@
+mahsulot = input()
+narx = int(input())
+son = int(input())
+
+jami = narx * son
+
+print("===== CHEK =====")
+print(f"Mahsulot: {mahsulot}")
+print(f"Narx: {narx}")
+print(f"Soni: {son}")
+print(f"Jami: {jami}")
+print("================")
