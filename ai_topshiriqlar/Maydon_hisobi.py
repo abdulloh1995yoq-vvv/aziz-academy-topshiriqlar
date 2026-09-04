74be7f30d3afcfb21b350fcc417cd03ce6be43d4
abdulloh1995yoq-vvv <abdulloh1995yoq@gmail.com>
2026-09-04 13:43:03 +0500

    Add AI task: Maydon hisobi

---
+++ ai_topshiriqlar/Maydon_hisobi.py
@@ -0,0 +1,17 @@
+# Maydon hisobi
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+qolgan_yuza = (a * b) - (c * d)
+
+katta_perimetri = 2 * (a +b)
+
+print(qolgan_yuza)
+print(katta_perimetri)
