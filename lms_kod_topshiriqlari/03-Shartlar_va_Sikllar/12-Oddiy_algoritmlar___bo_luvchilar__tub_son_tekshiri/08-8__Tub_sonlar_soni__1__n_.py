6c1bb4b2dfa2864a9af346d90280c794b0f9c0b9
abdulloh1995yoq-vvv <abdulloh1995yoq@gmail.com>
2026-09-15 11:32:12 +0500

    Add: 8) Tub sonlar soni (1..n) [abdulloh1995yoq-vvv]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/08-8__Tub_sonlar_soni__1__n_.py
@@ -0,0 +1,11 @@
+n = int(input())
+count = 0
+
+for num in range(2, n + 1):
+    for i in range(2, num):
+        if num % i == 0:\
+            break
+    else:
+        count += 1
+        
+print(count)
