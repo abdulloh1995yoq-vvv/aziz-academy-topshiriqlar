b0709f1db7893483d33fedd747f90e4d97e1598a
abdulloh1995yoq-vvv <abdulloh1995yoq@gmail.com>
2026-09-16 14:25:15 +0500

    Add: 13) Guess game (manfiy sonlar bilan) [abdulloh1995yoq-vvv]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/13-13__Guess_game__manfiy_sonlar_bilan_.py
@@ -0,0 +1,11 @@
+yashirin_son = -4
+
+while True:
+    son = int(input())
+    if son > yashirin_son:
+        print("High")
+    elif son < yashirin_son:
+        print("Low")
+    else:
+        print("Correct")
+        break
