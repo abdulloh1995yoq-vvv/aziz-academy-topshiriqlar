89a595cf12ddee9f20a315e07edf577dd6cf8ca6
abdulloh1995yoq-vvv <abdulloh1995yoq@gmail.com>
2026-09-16 14:12:45 +0500

    Add: 14) Cheksiz urinish, lekin hisob bilan [abdulloh1995yoq-vvv]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/14-14__Cheksiz_urinish__lekin_hisob_bilan.py
@@ -0,0 +1,10 @@
+yashirin_son = 11
+urinishlar_soni = 0
+
+while True:
+    son = int(input())
+    urinishlar_soni += 1
+    if son == yashirin_son:
+        break
+        
+print(urinishlar_soni)
